927c835515b2ab77ad19eec23f8fd98bd4652638
DLRibeiro <dlr4@cin.ufpe.br>
2018-11-05 20:31:59 -0300

    Alterando para ser possivel serializar por json

--- SOAS/ServicoCadastroUsuario/src/main/java/cadastroUsuario/domain/restaurante/persistidas/Pedido.java
+++ SOAS/ServicoCadastroUsuario/src/main/java/cadastroUsuario/domain/restaurante/persistidas/Pedido.java
@@ -1,15 +1,31 @@
 package cadastroUsuario.domain.restaurante.persistidas;
 
 import cadastroUsuario.domain.restaurante.Preco;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import java.util.List;
+import org.hibernate.validator.constraints.NotEmpty;
 
 public class Pedido{
 
+  @NotEmpty
   private List<Prato> pratos;
+
+  @NotEmpty
   private Preco precoPedido;
+
+  @NotEmpty
   private int usuarioPedido;
 
-  public Pedido(List<Prato> pratos, int usuarioPedido){
+  public Pedido(){
+
+  }
+  @JsonCreator
+  public Pedido(
+      @JsonProperty("pratos") List<Prato> pratos, @JsonProperty("usuarioPedido") int usuarioPedido){
     this.pratos = pratos;
     Preco precoPedido = new Preco(0,0);
     for(Prato prato: pratos){
@@ -18,4 +34,33 @@ public class Pedido{
     this.usuarioPedido = usuarioPedido;
   }
 
+  @JsonGetter("pratos")
+  public List<Prato> getPratos() {
+    return pratos;
+  }
+
+  @JsonSetter("pratos")
+  public void setPratos(List<Prato> pratos) {
+    this.pratos = pratos;
+  }
+
+  @JsonGetter("precoPedido")
+  public Preco getPrecoPedido() {
+    return precoPedido;
+  }
+
+  @JsonSetter("precoPedido")
+  public void setPrecoPedido(Preco precoPedido) {
+    this.precoPedido = precoPedido;
+  }
+
+  @JsonGetter("usuarioPedido")
+  public int getUsuarioPedido() {
+    return usuarioPedido;
+  }
+
+  @JsonSetter("usuarioPedido")
+  public void setUsuarioPedido(int usuarioPedido) {
+    this.usuarioPedido = usuarioPedido;
+  }
 }
--- SOAS/ServicoCadastroUsuario/src/main/java/cadastroUsuario/domain/restaurante/persistidas/Prato.java
+++ SOAS/ServicoCadastroUsuario/src/main/java/cadastroUsuario/domain/restaurante/persistidas/Prato.java
@@ -1,26 +1,68 @@
 package cadastroUsuario.domain.restaurante.persistidas;
 
 import cadastroUsuario.domain.restaurante.Preco;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import org.hibernate.validator.constraints.NotEmpty;
 
 public abstract class Prato{
 
+  @NotEmpty
   private String descricao;
+  @NotEmpty
   private String nome;
+  @NotEmpty
   private int tempo;
+  @NotEmpty
   private Preco preco;
 
-  public Prato(String nome, String descricao, int tempo, int reais, int centavos){
+  public Prato(){
+
+  }
+  @JsonCreator
+  public Prato(@JsonProperty("nome") String nome,
+      @JsonProperty("descricao") String descricao,
+      @JsonProperty("tempo") int tempo,
+      @JsonProperty("preco") Preco preco){
     this.nome = nome;
     this.descricao = descricao;
     this.tempo = tempo;
-    this.preco = new Preco(reais, centavos);
+    this.preco = preco;
   }
 
+  @JsonGetter("preco")
   public Preco getPreco(){
     return this.preco;
   }
 
+  @JsonSetter("preco")
   public int getTempo(){
     return this.tempo;
+  }
+  @JsonGetter("descricao")
+  public String getDescricao() {
+    return descricao;
+  }
+  @JsonSetter("descricao")
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+  @JsonGetter("name")
+  public String getNome() {
+    return nome;
+  }
+  @JsonSetter("name")
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  @JsonSetter("tempo")
+  public void setTempo(int tempo) {
+    this.tempo = tempo;
+  }
+  @JsonSetter("preco")
+  public void setPreco(Preco preco) {
+    this.preco = preco;
   }
 }
